afa76b944a113ed8ac661e197c55072fef5cae34
Dan Brewster <dnbrwstr@gmail.com>
2015-10-06 22:09:21 -0400

    Note about permissions request triggered by seedContacts

--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
+/**
+ * Note: enabling seedAddressBook here will trigger
+ * a contacts permissions request on app startup
+ */
 export default {
   serverRoot: 'http://192.168.1.5:3000',
   logState: false,

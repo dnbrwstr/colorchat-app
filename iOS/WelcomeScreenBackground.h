9a9ebe14026e8653d839c89d619c21dde8c2602f
Dan Brewster <dnbrwstr@gmail.com>
2015-11-24 19:16:44 -0500

    Testing particle-based welcome screen

---
+++ iOS/WelcomeScreenBackground.h
@@ -0,0 +1,19 @@
+//
+//  WelcomeScreenBackground.h
+//  ColorChat
+//
+//  Created by Daniel Brewster on 11/23/15.
+//
+
+#ifndef WelcomeScreenBackground_h
+#define WelcomeScreenBackground_h
+
+
+#endif /* WelcomeScreenBackground_h */
+
+@interface WelcomeScreenBackground : UIView
+
+@property (strong, nonatomic) NSMutableArray *stars;
+@property (strong, nonatomic) CALayer *backdrop;
+@property (strong, nonatomic) CADisplayLink *link;
+@end
--- js/components/WelcomeScreen.js
+++ js/components/WelcomeScreen.js
@@ -5,35 +5,36 @@ import PressableView from './PressableView';
 import LoaderButton from './LoaderButton';
 import { navigateTo } from '../actions/NavigationActions';
 import { appName } from '../config';
+import BaseText from './BaseText';
 
 let {
   View,
-  Text
+  Text,
+  requireNativeComponent
 } = React;
+
+let WelcomeScreenBackground = requireNativeComponent('RCTWelcomeScreenBackground', null);
 
 let WelcomeScreen = React.createClass({
   render: function () {
     return (
       <View style={style.wrapper}>
-        <View style={style.content}>
-          <Text style={style.text}>
-            <Text>Hi{"\n\n"}</Text>
-            <Text>Welcome to {appName} beta{"\n\n"}</Text>
-            <Text>{appName} allows you to send blocks of color to your friends{"\n\n"}</Text>
-            <Text>{appName} authenticates you using your phone number and allows you to chat with contacts who also have the app installed{"\n\n"}</Text>
-            <Text>Please let me know if you have any questions or suggestions, either by email or on Github{"\n\n"}</Text>
-            <Text>Enjoy :)</Text>
-          </Text>
+        <WelcomeScreenBackground style={style.welcome} />
+        <View style={style.titleContainer}>
+          <BaseText style={style.title}>ColorChat</BaseText>
         </View>
-
-        <LoaderButton
-          style={style.submit}
-          loading={false}
-          onPress={this.onPressNext}
-          message="Get started"
-        />
+        <View style={style.floatMessageContainer}>
+          <View style={style.floatMessage}>
+            <BaseText style={style.floatMessageText}>Chat with{"\n"}colors instead{"\n"}of words</BaseText>
+          </View>
+        </View>
+        <View style={style.bottomBar}>
+          <PressableView style={style.bottomBarButton} onPress={this.onPressNext}>
+            <BaseText style={style.bottomBarButtonText}>Setup</BaseText>
+          </PressableView>
+        </View>
       </View>
-    )
+    );
   },
 
   onPressNext: function () {
@@ -47,29 +48,64 @@ let style = Style.create({
     backgroundColor: '#f8f8f8',
     flex: 1
   },
-  content: {
-    ...Style.mixins.contentWrapperBase,
-    paddingRight: Style.values.outerPadding - 4
+  welcome: {
+    flex: 1
   },
-  text: {
-    ...Style.mixins.textBase
-  },
-  startButtonWrapper: {
-    alignItems: 'stretch',
-    justifyContent: 'center'
-  },
-  startButton: {
-    borderTopColor: '#BABABA',
-    borderTopWidth: 1,
+  titleContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
     height: Style.values.rowHeight,
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center'
   },
-  startButtonActive: {
-    backgroundColor: '#EFEFEF'
+  title: {
+    color: 'white',
+    padding: 20,
+    textAlign: 'center'
   },
-  startButtonText: {
-    ...Style.mixins.textBase
+  bottomBar: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: Style.values.rowHeight,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    alignItems: 'stretch'
+  },
+  bottomBarButton: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  bottomBarButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  floatMessageContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+  floatMessage: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    color: '#999'
+  },
+  floatMessageText: {
+    color: 'white',
+    textAlign: 'center'
   }
 });
 

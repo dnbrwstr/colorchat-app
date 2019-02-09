52838836744d31a8f0009d96e098f128ff5cb306
Dan Brewster <dnbrwstr@gmail.com>
2019-02-09 14:16:39 -0500

    Fix ContactList offset for Android

--- js/components/ContactList.js
+++ js/components/ContactList.js
@@ -1,14 +1,9 @@
-import React from "react";
-import { View, FlatList, Text, PixelRatio, ScrollView } from "react-native";
-import Color from "color";
-import PressableView from "./PressableView";
+import React, { Component } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
 import Style from "../style";
-import BaseText from "./BaseText";
 import ContactListItem from "./ContactListItem";
 
-const BR = "\n";
-
-export default (ContactList = class extends React.Component {
+const ContactList = class extends Component {
   static defaultProps = (function() {
     onSelect: () => {};
   })();
@@ -16,8 +11,8 @@ export default (ContactList = class extends React.Component {
   render() {
     return (
       <FlatList
+        contentContainerStyle={styles.contentContainer}
         data={this.props.contacts}
-        renderScrollComponent={this.renderScrollComponent}
         renderItem={this.renderContact}
         renderSeperator={this.renderSeperator}
         keyExtractor={c => c.recordID}
@@ -32,24 +27,6 @@ export default (ContactList = class extends React.Component {
       />
     );
   }
-
-  renderScrollComponent = props => {
-    let inset = {
-      top: Style.values.rowHeight,
-      right: 0,
-      bottom: 0,
-      left: 0
-    };
-
-    let offset = {
-      x: 0,
-      y: -Style.values.rowHeight
-    };
-
-    return (
-      <ScrollView {...props} contentInset={inset} contentOffset={offset} />
-    );
-  };
 
   renderContact = ({ index, item }) => {
     return (
@@ -68,4 +45,12 @@ export default (ContactList = class extends React.Component {
   onSelectContact = contact => {
     this.props.onSelect(contact);
   };
+};
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingTop: Style.values.rowHeight
+  }
 });
+
+export default ContactList;

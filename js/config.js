d1ec18be3e96771031d0c0bacca42efdfe3ebd54
Dan Brewster <dnbrwstr@gmail.com>
2019-02-10 13:31:09 -0500

    Separate dev + production config

--- js/config.js
+++ js/config.js
@@ -2,16 +2,35 @@
  * Note: enabling seedAddressBook here will trigger
  * a contacts permissions request on app startup
  */
-export default {
-  appName: "Color Chat",
-  inviteLink: "http://soft.works/colorchat",
-  serverRoot: "http://colorchat-server.color.slow.so",
-  logState: false,
-  logActions: false,
-  saveState: true,
-  rehydrate: true,
-  rehydrateBlacklist: ["ui", "navigation", "notifications", "messages"],
-  seedAddressBook: false,
-  seedMessages: false,
-  seedMessageCount: 10
+const config = {
+  development: {
+    appName: "Color Chat",
+    inviteLink: "http://soft.works/colorchat",
+    serverRoot: "http://192.168.1.3:3000",
+    logState: false,
+    logActions: false,
+    saveState: true,
+    rehydrate: true,
+    rehydrateBlacklist: ["ui", "navigation", "notifications", "messages"],
+    seedAddressBook: false,
+    seedMessages: false,
+    seedMessageCount: 10
+  },
+  production: {
+    appName: "Color Chat",
+    inviteLink: "http://soft.works/colorchat",
+    serverRoot: "http://colorchat-server.color.slow.so",
+    logState: false,
+    logActions: false,
+    saveState: true,
+    rehydrate: true,
+    rehydrateBlacklist: ["ui", "navigation", "notifications", "messages"],
+    seedAddressBook: false,
+    seedMessages: false,
+    seedMessageCount: 10
+  }
 };
+
+const key = __DEV__ ? "development" : "production";
+
+export default config[key];

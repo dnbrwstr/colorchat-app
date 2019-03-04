83854141926ba3fb50d372598db1277b8798575d
Dan Brewster <dnbrwstr@gmail.com>
2019-03-04 15:54:33 -0500

    Change server root

--- js/config.js
+++ js/config.js
@@ -6,25 +6,41 @@ const config = {
   development: {
     appName: "Color Chat",
     inviteLink: "http://soft.works/colorchat",
-    serverRoot: "http://192.168.1.3:3000",
+    // serverRoot: "http://192.168.86.247:3000",
+    // serverRoot: "http://192.168.1.11:3000",
+    serverRoot: "https://colorchat-core.soft.works",
     logState: false,
     logActions: false,
     saveState: true,
     rehydrate: true,
-    rehydrateBlacklist: ["ui", "navigation", "notifications", "messages"],
+    rehydrateBlacklist: [
+      "ui",
+      "notifications",
+      "messages",
+      // "theme",
+      "signup",
+      // "user",
+      "contacts"
+    ],
     seedAddressBook: false,
     seedMessages: false,
     seedMessageCount: 10
   },
   production: {
     appName: "Color Chat",
-    inviteLink: "http://soft.works/colorchat",
-    serverRoot: "http://colorchat-server.color.slow.so",
+    inviteLink: "https://soft.works/colorchat",
+    serverRoot: "https://colorchat-core.soft.works",
     logState: false,
     logActions: false,
     saveState: true,
     rehydrate: true,
-    rehydrateBlacklist: ["ui", "navigation", "notifications", "messages"],
+    rehydrateBlacklist: [
+      "ui",
+      "navigation",
+      "notifications",
+      "messages",
+      "signup"
+    ],
     seedAddressBook: false,
     seedMessages: false,
     seedMessageCount: 10
